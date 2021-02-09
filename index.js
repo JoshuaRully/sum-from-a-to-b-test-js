7258baac3bf5c52ee95134c42d2728baecd58949
Joshua Rully <joshrully@gmail.com>
2021-02-09 03:28:16 +0000

    solved!

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = (a, b) => {
+  if (a === b) {
+    return b;
+  } else {
+    return a + sum(a + 1, b);
+  }
+};
 
 module.exports = sum;
